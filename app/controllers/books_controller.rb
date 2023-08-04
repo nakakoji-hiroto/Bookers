387b905fe4589643d043d77b66286ec9ab08f789
EC2 Default User <ec2-user@ip-172-31-1-221.ap-northeast-1.compute.internal>
2023-08-04 01:11:04 +0000

    [Finish] create機能作成

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -5,10 +5,28 @@ class BooksController < ApplicationController
   def index
     @book = Book.new
   end
-
+  
+  def create
+    @book = Book.new(list_params)
+    if @book.save
+      flash[:notice] = 'Book was successfully created.'
+      redirect_to book_path(@book.id)
+    else
+      flash.now[:alert] = 'errors prohibited this book from being saved:'
+      render :intex
+    end
+  end
+  
   def show
+    @book = Book.find(params[:id])
   end
 
   def edit
+  end
+  
+  private
+  
+  def list_params
+    params.require(:book).permit(:title, :body)
   end
 end
